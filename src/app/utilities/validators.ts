ac4bb2e43aa7bf43ed4fc37b8ef8e9be631b65b9
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-07-08 00:24:05 -0500

    Variables moved to an own global file for constants

--- src/app/utilities/validators.ts
+++ src/app/utilities/validators.ts
@@ -1,9 +1,5 @@
 import * as moment from 'moment';
-
-const DATE_FORMAT = 'YYYY-MM-DD';
-const LICENSE_PLATE_REGEX = /^[A-Za-z]{3}-\d{3,4}$/
-const TIME_REGEX = /^[012345]\d:[012345]\d$/;
-
+import {DATE_FORMAT, LICENSE_PLATE_REGEX, TIME_REGEX} from "./global";
 
 export function validateLicensePlate(licensePlate: string): boolean {
     if (typeof licensePlate !== 'string') {
