8c9b5f7145903196275c22471829a8a40cd2c7f7
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-07-08 00:23:34 -0500

    Util methods for obtain a weekday from a date

---
+++ src/app/utilities/util.ts
@@ -0,0 +1,30 @@
+import * as moment from 'moment';
+import {Weekday} from "../enum/Weekday";
+import {DATE_FORMAT} from "./global";
+
+
+function getWeekday(date: string): Weekday {
+  try {
+    let isoWeekday: number = moment(date, DATE_FORMAT, true).isoWeekday();
+    switch(isoWeekday) {
+      case Weekday.MONDAY:
+        return Weekday.MONDAY;
+      case Weekday.TUESDAY:
+        return Weekday.TUESDAY;
+      case Weekday.WEDNESDAY:
+        return Weekday.WEDNESDAY;
+      case Weekday.THURSDAY:
+        return Weekday.THURSDAY;
+      case Weekday.FRIDAY:
+        return Weekday.FRIDAY;
+      default:
+        return null;
+    }
+  } catch (e) {
+    return null;
+  }
+}
+
+export const Utils = {
+  getWeekday: getWeekday
+}
