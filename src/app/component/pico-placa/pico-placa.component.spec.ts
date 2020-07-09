cbbc6f06a7321ba604386d003f15979258c53ee3
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-07-08 21:47:44 -0500

    Test cases added for pico-placa component

--- src/app/component/pico-placa/pico-placa.component.spec.ts
+++ src/app/component/pico-placa/pico-placa.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PicoPlacaComponent } from './pico-placa.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 describe('PicoPlacaComponent', () => {
   let component: PicoPlacaComponent;
@@ -8,7 +9,8 @@ describe('PicoPlacaComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PicoPlacaComponent ]
+      declarations: [ PicoPlacaComponent ],
+      imports: [ReactiveFormsModule]
     })
     .compileComponents();
   }));
@@ -21,5 +23,16 @@ describe('PicoPlacaComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it ('should have invalid form when invalid data is typed', () => {
+    component.predictForm.controls['time'].setValue('23-asd');
+    expect(component.predictForm.invalid);
+  });
+
+  it ('should have date with required and valid date errors', () => {
+    expect(component.predictForm.controls['date'].errors.required).toBeTruthy();
+    component.predictForm.controls['date'].setValue('as1j1aa  ');
+    expect(component.predictForm.controls['date'].errors.invalidDate).toBeTruthy();
   });
 });
