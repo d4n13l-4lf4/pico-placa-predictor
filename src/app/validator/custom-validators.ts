5582da60840b20644c461bf31767c15b678b00b2
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-07-08 20:58:14 -0500

    DateValidators and LicensePlateValidators imported

--- src/app/validator/custom-validators.ts
+++ src/app/validator/custom-validators.ts
@@ -1,10 +1,10 @@
 import {AbstractControl, ValidatorFn} from "@angular/forms";
-import {validateDate, validateLicensePlate, validateTime} from "../utilities/validators";
+import {DateValidators, LicensePlateValidators} from "../utilities/validators";
 
 function licensePlateValidator(): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} | null => {
     const licensePlate: string = control.value;
-    const valid: boolean = validateLicensePlate(licensePlate);
+    const valid: boolean = LicensePlateValidators.validateLicensePlate(licensePlate);
     return !valid ? {'invalidLicense': {value: control.value }} : null;
   }
 }
@@ -12,16 +12,15 @@ function licensePlateValidator(): ValidatorFn {
 function dateValidator(): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} | null => {
     const date: string = control.value;
-    const valid: boolean = validateDate(date);
+    const valid: boolean = DateValidators.validateDate(date);
     return !valid ? {'invalidDate': {value: control.value}} : null;
   }
 }
 
-
 function timeValidator(): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} | null => {
     const time: string = control.value;
-    const valid: boolean = validateTime(time);
+    const valid: boolean = DateValidators.validateTime(time);
     return !valid ? {'invalidTime': {value: control.value}} : null;
   }
 }
