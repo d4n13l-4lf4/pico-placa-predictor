99a1e56dad01e04e07dea23a9f1648aa4050d7ed
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-07-08 20:58:53 -0500

    Test suite name changed and tests were added

--- src/app/service/predict-data.service.spec.ts
+++ src/app/service/predict-data.service.spec.ts
@@ -2,11 +2,27 @@ import { TestBed } from '@angular/core/testing';
 
 import { PredictDataService } from './predict-data.service';
 
-describe('WeekdayLicenseService', () => {
+describe('PredictDataService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service: PredictDataService = TestBed.get(PredictDataService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return predict data', (done: DoneFn) => {
+    const service: PredictDataService = TestBed.get(PredictDataService);
+    service
+      .getPredictData()
+      .subscribe(
+        data => {
+          expect(data).toBeTruthy();
+          done();
+        },
+        _ => {
+          expect(false).toBeTruthy();
+          done();
+        }
+      )
   });
 });
