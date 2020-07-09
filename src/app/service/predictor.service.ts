2a1bfc1d4753f0baa7b6015b2b95c782f7ef6cd1
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-07-08 20:54:08 -0500

    DateValidators added to validate time between

--- src/app/service/predictor.service.ts
+++ src/app/service/predictor.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import {PredictDataService} from './predict-data.service';
 import {LicensePlatePredict} from '../model/LicensePlatePredict';
 import {Utils} from '../utilities/util';
-import {timeBetween} from '../utilities/validators';
+import {DateValidators} from '../utilities/validators';
+import {Weekday} from "../enum/Weekday";
 
 @Injectable({
   providedIn: 'root'
@@ -17,15 +18,19 @@ export class PredictorService {
 
       predictDataSubscriber.subscribe(
         predictData => {
+          const forbidden = false;
           const weekdayLicenses = predictData.weekdayLicenses;
           const forbiddenSlots = predictData.forbiddenSlots;
 
-          if (!forbiddenSlots && !weekdayLicenses) {
+          if (!forbiddenSlots || !weekdayLicenses || !licensePlatePredict) {
             return reject(`Error: couldn't get data for validation`);
           }
 
-          const weekdayToCheck = Utils.getWeekday(licensePlatePredict.date);
+          const weekdayToCheck: Weekday | {error?: string} = Utils.getWeekday(licensePlatePredict.date);
           const lastDigit = licensePlatePredict.licensePlate.slice(-1);
+
+          if (typeof weekdayToCheck === 'object')
+            return reject(`Error: Couldn't get weekday from date ${licensePlatePredict.date}`)
 
           const checkSlots = weekdayLicenses
             .filter(w => w.weekday === weekdayToCheck && w.lastLicenseDigit === Number(lastDigit))
@@ -34,7 +39,7 @@ export class PredictorService {
           if (checkSlots) {
             const forbidden = forbiddenSlots
               .reduce((ac, cv) => {
-                  const comparison = timeBetween(licensePlatePredict.time, cv.startTime, cv.endTime);
+                  const comparison: {error?: string} | boolean = DateValidators.timeBetween(licensePlatePredict.time, cv.startTime, cv.endTime);
                   if (typeof comparison === 'object') {
                     return ac || true;
                   }
@@ -46,7 +51,7 @@ export class PredictorService {
             return resolve(forbidden);
           }
 
-          return resolve(false);
+          return resolve(forbidden);
         },
         error => {
           reject(`Error: ${error}`);
