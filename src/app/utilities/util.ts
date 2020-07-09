96b98aa079d35d4ab0153dcf5ceae1e1b80705e3
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-07-08 20:56:04 -0500

    Returning error in case something bad happens

--- src/app/utilities/util.ts
+++ src/app/utilities/util.ts
@@ -3,7 +3,7 @@ import {Weekday} from '../enum/Weekday';
 import {DATE_FORMAT} from './global';
 
 
-function getWeekday(date: string): Weekday {
+function getWeekday(date: string): {error?: string} | Weekday {
   try {
     const isoWeekday: number = moment(date, DATE_FORMAT, true).isoWeekday();
     switch(isoWeekday) {
@@ -18,10 +18,10 @@ function getWeekday(date: string): Weekday {
       case Weekday.FRIDAY:
         return Weekday.FRIDAY;
       default:
-        return null;
+        return {error: `Couldn't match a valid Weekday`};
     }
   } catch (e) {
-    return null;
+    return {error: e};
   }
 }
 
