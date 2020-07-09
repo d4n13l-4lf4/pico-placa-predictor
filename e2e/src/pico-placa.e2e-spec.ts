47b0638d5f9809279c042ed91906e7722ac22847
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-07-08 21:53:10 -0500

    Test cases were added for valid and invalid input. Also, there are test cases for checking expected output after predicting.

--- e2e/src/pico-placa.e2e-spec.ts
+++ e2e/src/pico-placa.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { browser, logging } from 'protractor';
 import {PicoPlacaPage} from "./pico-placa.po";
 
-describe('Pico Placa e2e', () => {
+describe('Pico Placa Test Suite', () => {
   let page: PicoPlacaPage;
 
   beforeEach(() => {
@@ -13,6 +13,75 @@ describe('Pico Placa e2e', () => {
     expect(page.getCardTitleText()).toEqual('Pico y Placa Predictor');
   });
 
+
+  it ('should have invalid inputs when invalid data is typed', () => {
+    page.navigateTo();
+    const licensePlateInput = page.getLicensePlateInput();
+    const dateInput = page.getDateInput();
+    const timeInput = page.getTimeInput();
+
+    licensePlateInput.sendKeys('as.s-d.');
+    dateInput.sendKeys('202021-');
+    timeInput.sendKeys('saslñ-.');
+
+    Promise.all([
+      expect(licensePlateInput.getAttribute('class')).toContain('ng-invalid'),
+      expect(dateInput.getAttribute('class')).toContain('ng-invalid'),
+      expect(timeInput.getAttribute('class')).toContain('ng-invalid'),
+    ]);
+  });
+
+  it ('should have valid inputs when valid data is typed', () => {
+    page.navigateTo();
+    const licensePlateInput = page.getLicensePlateInput();
+    const dateInput = page.getDateInput();
+    const timeInput = page.getTimeInput();
+
+    licensePlateInput.sendKeys('ABC-1234');
+    dateInput.sendKeys('2020-01-01');
+    timeInput.sendKeys('07:30');
+
+    Promise.all([
+      expect(licensePlateInput.getAttribute('class')).toContain('ng-valid'),
+      expect(dateInput.getAttribute('class')).toContain('ng-valid'),
+      expect(timeInput.getAttribute('class')).toContain('ng-valid'),
+    ]);
+  });
+
+
+  it ('should show the message that you are fine driving', () => {
+    page.navigateTo();
+    const expectedMessage = `You are fine driving your car at that time!`;
+    const licensePlateInput = page.getLicensePlateInput();
+    const dateInput = page.getDateInput();
+    const timeInput = page.getTimeInput();
+    const predictButton = page.getPredictButton();
+
+    licensePlateInput.sendKeys('ABC-1234');
+    dateInput.sendKeys('2020-08-11');
+    timeInput.sendKeys('23:21');
+    predictButton.click();
+
+    const message = page.getPermittedMessage();
+    expect(message).toBe(expectedMessage);
+  });
+
+  it ('should show the message that you shouldn\'t drive', () => {
+    page.navigateTo();
+    const expectedMessage = `You shouldn't drive your car at that time!`;
+    const licensePlateInput = page.getLicensePlateInput();
+    const dateInput = page.getDateInput();
+    const timeInput = page.getTimeInput();
+    const predictButton = page.getPredictButton();
+
+    licensePlateInput.sendKeys('ABC-1235');
+    dateInput.sendKeys('2020-07-08');
+    timeInput.sendKeys('07:30');
+    predictButton.click();
+
+    const message = page.getForbiddenMessage();
+    expect(message).toBe(expectedMessage);
+  });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
