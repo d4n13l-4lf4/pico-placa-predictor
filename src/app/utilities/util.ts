8c14bae0edc0ce42b3985c4ed42521989804be9c
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-07-08 21:45:51 -0500

    Weekday enum members refactoring

--- src/app/utilities/util.ts
+++ src/app/utilities/util.ts
@@ -7,16 +7,16 @@ function getWeekday(date: string): {error?: string} | Weekday {
   try {
     const isoWeekday: number = moment(date, DATE_FORMAT, true).isoWeekday();
     switch(isoWeekday) {
-      case Weekday.MONDAY:
-        return Weekday.MONDAY;
-      case Weekday.TUESDAY:
-        return Weekday.TUESDAY;
-      case Weekday.WEDNESDAY:
-        return Weekday.WEDNESDAY;
-      case Weekday.THURSDAY:
-        return Weekday.THURSDAY;
-      case Weekday.FRIDAY:
-        return Weekday.FRIDAY;
+      case Weekday.Monday:
+        return Weekday.Monday;
+      case Weekday.Tuesday:
+        return Weekday.Tuesday;
+      case Weekday.Wednesday:
+        return Weekday.Wednesday;
+      case Weekday.Thursday:
+        return Weekday.Thursday;
+      case Weekday.Friday:
+        return Weekday.Friday;
       default:
         return {error: `Couldn't match a valid Weekday`};
     }
